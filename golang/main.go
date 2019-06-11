e3edf737d32031577f8ef397238aae19c59ee78c
twogg-git <cruz.catherin@gmail.com>
2019-06-10 22:46:48 -0500

    Adding new images and Dockerfiles
    Adding resources and docker file setup

---
+++ golang/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	html := `<!DOCTYPE html><html><body><center>
+		<img src="https://raw.githubusercontent.com/twogg-git/k8s-workshop/master/src/0.0.png">
+		<h1 style="color:blue">Baby steps with Docker Registry!</h1>
+        </center></body></html>`
+	fmt.Fprintf(w, html)
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
